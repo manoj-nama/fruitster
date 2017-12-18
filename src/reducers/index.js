8092456438e8446a6743335dd51d3ca41d1dc976
Manoj Nama <manoj.nama@outlook.com>
2017-12-18 13:16:16 +0530

    Adding reducers for menu and cart screens

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import UserReducer from './user.reducer';
 import AuthReducer from './auth.reducer';
+import MenuReducer from './menu.reducer';
+import CartReducer from './cart.reducer';
 
 const reducers = combineReducers({
   user: UserReducer,
-  auth: AuthReducer
+  auth: AuthReducer,
+  menu: MenuReducer,
+  cart: CartReducer
 });
 
 export default reducers;

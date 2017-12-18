5e93810eed645a6ae65cb495943f0e777bcc0b33
Manoj Nama <manoj.nama@outlook.com>
2017-12-18 14:37:09 +0530

    Adding custom fonts, udpated tabbar styling

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Font } from 'expo';
 
 import store from './src/store';
 import RootNavigator from './src/navigator';
 
 export default class App extends React.Component {
+  state = {
+    fontsLoaded: false
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      'amarnath': require('./assets/fonts/Amaranth-Regular.ttf'),
+      'amarnath-bold': require('./assets/fonts/Amaranth-Bold.ttf'),
+      'amarnath-italic': require('./assets/fonts/Amaranth-Italic.ttf')
+    });
+    this.setState({ fontsLoaded: true });
+  }
+
   render() {
+    if (!this.state.fontsLoaded) {
+      return null;
+    }
     return (
       <Provider store={store}>
         <RootNavigator />
--- src/navigator/index.js
+++ src/navigator/index.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { StackNavigator, TabNavigator } from 'react-navigation';
+import { Ionicons as Icon } from '@expo/vector-icons';
 
 import CartScreen from '../views/screens/Cart.screen';
 import UserScreen from '../views/screens/User.screen';
@@ -13,35 +15,68 @@ const MenuTab = StackNavigator({
     screen: MenuScreen
   }
 }, {
-  navigationOptions: defaultStackNavigationOptions
-});
+    navigationOptions: defaultStackNavigationOptions
+  });
 
 const UserTab = StackNavigator({
   User: {
     screen: UserScreen
   }
 }, {
-  navigationOptions: defaultStackNavigationOptions
-});
+    navigationOptions: defaultStackNavigationOptions
+  });
 
 const CartTab = StackNavigator({
   Cart: {
     screen: CartScreen
   }
 }, {
-  navigationOptions: defaultStackNavigationOptions
-});
+    navigationOptions: defaultStackNavigationOptions
+  });
 
 const RootNavigator = TabNavigator({
   Menu: {
-    screen: MenuTab
-  },
-  User: {
-    screen: UserTab
+    screen: MenuTab,
+    navigationOptions: {
+      tabBarLabel: 'Menu',
+      tabBarIcon: ({ tintColor }) => <Icon name={'logo-buffer'} size={24} color={tintColor} />
+    }
   },
   Cart: {
-    screen: CartTab
-  }
-});
+    screen: CartTab,
+    navigationOptions: {
+      tabBarLabel: 'Cart',
+      tabBarIcon: ({ tintColor }) => <Icon name={'md-basket'} size={24} color={tintColor} />
+    }
+  },
+  User: {
+    screen: UserTab,
+    navigationOptions: {
+      tabBarLabel: 'Account',
+      tabBarIcon: ({ tintColor }) => <Icon name={'ios-contact'} size={24} color={tintColor} />
+    }
+  },
+}, {
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      inactiveTintColor: '#555',
+      activeTintColor: '#fff',
+      activeBackgroundColor: '#d22',
+      inactiveBackgroundColor: '#fff',
+      showIcon: true,
+      labelStyle: {
+        fontFamily: 'amarnath',
+        fontSize: 12
+      },
+      style: {
+        backgroundColor: '#fff',
+        borderTopWidth: 0,
+        shadowColor: '#555',
+        shadowRadius: 5,
+        shadowOpacity: 0.2,
+        elevation: 5,
+      }
+    }
+  });
 
 export default RootNavigator;
